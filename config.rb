9120982a310b95405954384578ad7307a36c3b04
Patrick Fulton <patrick@bearded.com>
2015-05-28 19:40:49 -0400

    Add bower config & package list.

--- config.rb
+++ config.rb
@@ -50,6 +50,12 @@ set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
 
+# Add bower's directory to sprockets asset path
+after_configuration do
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  sprockets.append_path File.join "#{root}", @bower_config["directory"]
+end
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
